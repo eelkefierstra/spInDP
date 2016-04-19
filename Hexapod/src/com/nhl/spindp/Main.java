a12587b3d13c2645092377eea2f15147cfa323d1
dudecake <coen_koomen@hotmail.com>
2016-04-19 11:51:53 +0200

    Begin interface with servo's

---
+++ Hexapod/src/com/nhl/spindp/Main.java
@@ -0,0 +1,31 @@
+package com.nhl.spindp;
+
+import jssc.*;
+import javax.xml.bind.DatatypeConverter;
+
+public class Main
+{
+	SerialPort sPort;
+	public static void main(String[] args) throws SerialPortException
+	{
+		/*
+		Main p = new Main();
+		p.sPort = new SerialPort("/dev/ttyAMA0");
+		p.sPort.setParams(1000000, 8, 1, 0);
+		p.sPort.openPort();
+		p.sPort.writeBytes(DatatypeConverter.parseHexBinary("FF FF 01 05 03 1E 32 03 A3"));*/
+		String[] portNames = SerialPortList.getPortNames();
+		if (portNames.length == 0)
+		{
+			System.out.println("No serial devices found");
+		}
+		else
+		{
+			for (String s : portNames)
+			{
+				System.out.println(s);
+			}
+		}
+		
+	}
+}
