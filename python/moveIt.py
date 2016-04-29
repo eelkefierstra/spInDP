98355225c3672d3240f0e54904731ad4785b1f2a
project pi <pi@raspberry.net>
2016-04-29 11:41:37 +0000

    rewrote some python

--- python/moveIt.py
+++ python/moveIt.py
@@ -3,14 +3,14 @@ from time import sleep
 
 servo = Ax12();
 stand=0
-id=3
+id=2
 while(1):
-	while(id<19):
+	while(id<3):
 		print(str(servo.move(id,stand)))
 		print('moved '+str(id))
 		sleep(0.5)
 		id=id+1
-	id=3
+	id=2
 	if(stand==0):
 		stand=512
 	else:
